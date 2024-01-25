03305d24a828bc76d9ed13d074199a32d0b8cc40
William Moore <w.moore@dundee.ac.uk>
2024-01-25 09:52:42 +0000

    Add coordinate_transformations to ezomero_import_zarr.py

--- ezomero_import_zarr.py
+++ ezomero_import_zarr.py
@@ -24,7 +24,18 @@ def test_import(conn):
     chunks = (1, 1, 1, 1024, 1024)
     store = parse_url(filename, mode="w").store
     root = zarr.group(store=store)
-    write_image(image=data, group=root, axes="tczyx", storage_options=dict(chunks=chunks))
+
+    coordinate_transformations = []
+    pix_sizes = [1, 1, 0.5, 0.25, 0.25]
+    # the default scaler creates 5 levels and downsamples by 2 in x and y
+    for path in range(0, 5):
+        coordinate_transformations.append([{"type": "scale", "scale": pix_sizes[:]}])
+        pix_sizes[-1] = pix_sizes[-1] * 2
+        pix_sizes[-2] = pix_sizes[-2] * 2
+
+    write_image(image=data, group=root, axes="tczyx", 
+                coordinate_transformations=coordinate_transformations,
+                storage_options=dict(chunks=chunks))
 
     # import...
     id_image = ezomero.ezimport(conn=conn, target=filename, dataset=451)
